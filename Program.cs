4be77777c85fb59803e6eef8d5db85c47b785b9f
goncharova11 <92858794+goncharova11@users.noreply.github.com>
2022-12-11 19:23:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Equation
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            do
+            {
+                Console.WriteLine("Введите значение a, не равное нулю");
+                double m = double.Parse(Console.ReadLine());
+                a = m;
+            }
+            while (a == 0);
+
+            Console.WriteLine("Введите значение b");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение c");
+            double c = double.Parse(Console.ReadLine());
+
+            double D = Diskriminant(a, b, c);
+
+            if (D > 0) SquareRoots(D, a, b);
+            if (D == 0) SquareRoot(a, b);
+            else Console.WriteLine("Нет корней");
+            
+        }
+        public static double Diskriminant (double a, double b, double c)
+        {
+            double D = b * b - 4 * a * c;
+            return D;
+        }
+               
+        public static double[] SquareRoots(double D, double a, double b) 
+        {
+            double[] result = new double[]
+                {
+                    (-b + Math.Sqrt(D)) / (2 * a), (-b - Math.Sqrt(D)) / (2 * a)
+                };
+
+            Console.WriteLine(Convert.ToString(result[0]));
+            Console.WriteLine(Convert.ToString(result[1]));
+            return result;
+        }
+        public static double SquareRoot(double a, double b)
+        { 
+            double x1= (-b) / (2 * a);
+            return x1;
+        }
+    }
+}
