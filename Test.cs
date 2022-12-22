d59733b5e51602fa640fd9b3738e66c8bded1e77
goncharova11 <92858794+goncharova11@users.noreply.github.com>
2022-12-22 22:46:15 +0300

    Create Test.cs

---
+++ Test.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NegativeDiscriminant()
+        {
+            double a = 2;
+            double b = 1;
+            double c = 1;
+            double expected = -7;
+            double actual = Equation.Diskriminant(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void PositiveDiscriminant()
+        {
+            double a = 3;
+            double b = 7;
+            double c = -6;
+            double expected = 121;
+            double actual = Equation.Diskriminant(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ZeroDiscriminant()
+        {
+            double a = 4;
+            double b = 4;
+            double c = 1;
+            double expected = 0;
+            double actual = Equation.Diskriminant(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
